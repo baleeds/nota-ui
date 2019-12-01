08ac121f8af92a4c300f2efcdec9622391186d95
Benjamin Leeds <baleeds@ncsu.edu>
2019-11-30 22:09:06 -0500

    add: Login link to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,5 +2,21 @@ import React from 'react';
 import { Page } from '../components/Page';
 
 export const HomePage: React.FC = () => {
-  return <Page>Home page</Page>;
+  if (!process.env.REACT_APP_API_URL) {
+    throw new Error('REACT_APP_API_URL is not set');
+  }
+
+  const loginUrl = process.env.REACT_APP_API_URL.replace(
+    'graphql',
+    'auth/google'
+  );
+
+  return (
+    <Page>
+      Home page
+      <div>
+        <a href={loginUrl}>Login</a>
+      </div>
+    </Page>
+  );
 };
