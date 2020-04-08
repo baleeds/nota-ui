521fa58e227832b9af8bbd830a4458118db84f11
Benjamin Leeds <baleeds@ncsu.edu>
2020-04-07 23:01:03 -0400

    add: Button handler to go to draft page

--- src/pages/ReadPage/VerseDetailsContent.tsx
+++ src/pages/ReadPage/VerseDetailsContent.tsx
@@ -4,9 +4,11 @@ import { Block } from '../../components/Block';
 import { OutlineButton } from '../../components/Buttons';
 import { Annotations } from '../../components/Annotations';
 import { usePassage } from '../../hooks/usePassage';
+import { useHistory } from 'react-router';
 
 export const VerseDetailsContent: React.FC = () => {
-  const { passageId } = usePassage();
+  const { passageId, bookName, chapterNumber, verseNumber } = usePassage();
+  const history = useHistory();
 
   return (
     <>
@@ -15,6 +17,11 @@ export const VerseDetailsContent: React.FC = () => {
         <OutlineButton
           style={{ display: 'block', width: '100%', margin: '24px 0' }}
           type="button"
+          onClick={() =>
+            history.push(
+              `/annotate/${bookName}/${chapterNumber}/${verseNumber}`
+            )
+          }
         >
           Write an annotation
         </OutlineButton>
